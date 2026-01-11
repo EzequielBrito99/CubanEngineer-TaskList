c507f92c29ba68bd9a455a349d6283f42056dcd2
Ezequiel Brito Navarro <ebritonavarro@gmail.com>
2026-01-11 13:47:20 -0500

    chore(storybook): configure path aliases and remove default boilerplate
    - Set up @ alias resolution in viteFinal.
    - Delete default Vite/Storybook example stories and assets.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,10 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
   "stories": [
@@ -11,6 +17,17 @@ const config: StorybookConfig = {
     "@storybook/addon-a11y",
     "@storybook/addon-docs"
   ],
-  "framework": "@storybook/react-vite"
+  "framework": "@storybook/react-vite",
+  viteFinal: async (config) => {
+    config.plugins?.push(tsconfigPaths());
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../src"),
+      };
+    }
+
+    return config;
+  },
 };
 export default config;
